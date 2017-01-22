5e723fa80e57c8b7ed2a3949e20cf50f3ee14161
Hitsub <hitsumabushis@gmail.com>
2017-01-22 23:55:33 +0900

    in progress

---
+++ Assets/Sciprts/enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+	float time = 0, span = 0.5f;
+	GameObject bullet;
+	// Use this for initialization
+	void Start () {
+		bullet = Resources.Load <GameObject> ("bullet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += 1f * Time.deltaTime;
+		if (time >= span) {
+			Instantiate (bullet);
+			time = 0;
+		}
+	}
+}
---
+++ Assets/Sciprts/section2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class section2 : MonoBehaviour {
+
+	Vector2 position; //二次元ベクトル型
+
+	void Update () {
+		position = transform.position;
+		if (Input.GetKey (KeyCode.UpArrow))
+			position.y += 2f * Time.deltaTime;
+		if (Input.GetKey (KeyCode.DownArrow))
+			position.y -= 2f * Time.deltaTime;
+		if (Input.GetKey (KeyCode.RightArrow))
+			position.x += 2f * Time.deltaTime;
+		if (Input.GetKey (KeyCode.LeftArrow))
+			position.x -= 2f * Time.deltaTime;
+		transform.position = position;
+	}
+}
