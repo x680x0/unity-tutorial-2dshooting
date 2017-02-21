35e17b8781777fd5eb4d0cc2f159a4bdf1b30eb2
Hitsub <hitsumabushis@gmail.com>
2017-02-22 03:04:34 +0900

    for Wiki No6

---
+++ Assets/Sciprts/Timer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	float timer = 0;
+	Text textComponent;
+
+	void Start (){
+		textComponent = this.GetComponent<Text> ();
+	}
+
+	void Update () {
+		timer += 1f * Time.deltaTime;
+		textComponent.text = timer.ToString("F0");
+	}
+}
